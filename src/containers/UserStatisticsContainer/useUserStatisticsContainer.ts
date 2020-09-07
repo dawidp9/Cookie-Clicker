6518c39993f13fa5e13a14de03c5a5ef1839ba35
dawidp9 <dawid.piwko.dp@gmail.com>
2020-09-07 19:20:27 +0200

    changed triggering level up action to mobx reaction

--- src/containers/UserStatisticsContainer/useUserStatisticsContainer.ts
+++ src/containers/UserStatisticsContainer/useUserStatisticsContainer.ts
@@ -1,16 +1,7 @@
 import { useRootStore } from '../../state/RootStateContext';
-import { useEffect } from 'react';
 
 export default () => {
     const { user } = useRootStore();
 
-    useEffect(() => {
-        if (user.allPoints >= user.nextLevelRequirePoints) {
-            user.setNextLevel();
-        }
-    }, [user.allPoints, user]);
-
-    return {
-        user,
-    };
+    return { user };
 };
--- src/state/user/store.ts
+++ src/state/user/store.ts
@@ -1,10 +1,21 @@
-import { observable, action } from 'mobx';
+import { observable, action, reaction } from 'mobx';
 
 class UserStore {
     @observable allPoints: number = 0;
     @observable multiplier: number = 1;
     @observable currentLevel: number = 1;
     @observable nextLevelRequirePoints: number = 10;
+
+    constructor() {
+        reaction(
+            () => this.allPoints,
+            (points) => {
+                if (points >= this.nextLevelRequirePoints) {
+                    this.setNextLevel();
+                }
+            },
+        );
+    }
 
     @action addPointByClick = () => {
         this.allPoints = this.allPoints + this.multiplier;
